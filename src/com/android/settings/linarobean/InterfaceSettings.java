4003d3669fd5d3d4c9a544d2a73b2b875a606c9e
tackleberry_crash <amras.tasartir77@gmail.com>
2013-06-25 22:17:59 +0300

    Navigation bar mods (Color,Button Color,Glow Color, Glow Times, Button Transp.) (2/2)
    Change-Id: I4668f9ef097f69438f6c4217b587dbdfa8f36610

--- src/com/android/settings/linarobean/InterfaceSettings.java
+++ src/com/android/settings/linarobean/InterfaceSettings.java
@@ -36,10 +36,14 @@ import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.Preference.OnPreferenceChangeListener; 
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup; 
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem; 
 import android.util.Log;
 import android.text.Spannable;
 
@@ -47,8 +51,13 @@ import com.android.settings.Utils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import android.widget.EditText;
+import com.android.settings.util.Helpers;
+import com.android.settings.widget.SeekBarPreference;
 
-public class InterfaceSettings extends SettingsPreferenceFragment {
+import net.margaritov.preference.colorpicker.ColorPickerPreference; 
+
+public class InterfaceSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
 
     private static final String TAG = "InterfaceSettings";
 
@@ -56,11 +65,20 @@ public class InterfaceSettings extends SettingsPreferenceFragment {
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String KEY_WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged"; 
     private static final String STATUS_BAR_AUTO_HIDE = "status_bar_auto_hide";
+    private static final String PREF_GLOW_TIMES = "glow_times"; 
+    private static final String PREF_NAV_COLOR = "nav_bar_color";
+    private static final String PREF_NAV_BUTTON_COLOR = "nav_button_color";
+    private static final String PREF_NAV_GLOW_COLOR = "nav_button_glow_color"; 
 
     private Preference mCustomLabel;
     private CheckBoxPreference mWakeUpWhenPluggedOrUnplugged; 
     private CheckBoxPreference mDisableFullscreenKeyboard; 
     private CheckBoxPreference mStatusBarAutoHide;
+    ListPreference mGlowTimes; 
+    ColorPickerPreference mNavigationBarColor;
+    ColorPickerPreference mNavigationBarButtonColor;
+    ColorPickerPreference mNavigationBarGlowColor; 
+    SeekBarPreference mButtonAlpha; 
 
     private String mCustomLabelText = null;
     private int newDensityValue;
@@ -89,7 +107,100 @@ public class InterfaceSettings extends SettingsPreferenceFragment {
         mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
         updateCustomLabelTextSummary();
 
+        mGlowTimes = (ListPreference) findPreference(PREF_GLOW_TIMES);
+        mGlowTimes.setOnPreferenceChangeListener(this);
+
+        updateGlowTimesSummary(); 
+	
+	refreshSettings(); 
+
     }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.interface_settings);
+
+        prefs = getPreferenceScreen();
+
+        mNavigationBarColor = (ColorPickerPreference) findPreference(PREF_NAV_COLOR);
+        mNavigationBarColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_TINT, 0xff000000);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mNavigationBarColor.setNewPreviewColor(intColor);
+
+
+        mNavigationBarGlowColor = (ColorPickerPreference) findPreference(PREF_NAV_GLOW_COLOR);
+        mNavigationBarGlowColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_GLOW_TINT, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mNavigationBarGlowColor.setNewPreviewColor(intColor);
+
+        mNavigationBarButtonColor = (ColorPickerPreference) findPreference(PREF_NAV_BUTTON_COLOR);
+        mNavigationBarButtonColor.setNewPreviewColor(0xffffffff);
+        mNavigationBarButtonColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_BUTTON_TINT, 0x00000000);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        if (intColor == 0x00000000) {
+            mNavigationBarButtonColor.setSummary(getResources().getString(R.string.none));
+        } else {
+            mNavigationBarButtonColor.setNewPreviewColor(intColor);
+        }
+
+        float defaultAlpha;
+        try{
+            defaultAlpha = Settings.System.getFloat(getActivity()
+                     .getContentResolver(), Settings.System.NAVIGATION_BAR_BUTTON_ALPHA);
+        } catch (Exception e) {
+            defaultAlpha = 0.3f;
+                     Settings.System.putFloat(getActivity().getContentResolver(), Settings.System.NAVIGATION_BAR_BUTTON_ALPHA, 0.3f);
+        }
+        mButtonAlpha = (SeekBarPreference) findPreference("button_transparency");
+        mButtonAlpha.setProperty(Settings.System.NAVIGATION_BAR_BUTTON_ALPHA);
+        mButtonAlpha.setInitValue((int) (defaultAlpha * 100));
+        mButtonAlpha.setOnPreferenceChangeListener(this);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.nav_bar_style_dimen, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.reset:
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.NAVIGATION_BAR_TINT, 0xff000000);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.NAVIGATION_BAR_BUTTON_TINT, 0x00000000);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                        Settings.System.NAVIGATION_BAR_GLOW_TINT, 0xffffffff);
+
+                Settings.System.putFloat(getActivity().getContentResolver(),
+                       Settings.System.NAVIGATION_BAR_BUTTON_ALPHA, 0.3f);
+
+                refreshSettings();
+                return true;
+            
+             default:
+                return super.onContextItemSelected(item);
+        }
+    } 
+
+    private void updateNavbarPreferences( boolean show ) {
+        mGlowTimes.setEnabled(show); 
+    } 
 
     private void updateCustomLabelTextSummary() {
         mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
@@ -111,6 +222,77 @@ public class InterfaceSettings extends SettingsPreferenceFragment {
         super.onPause();
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGlowTimes) {
+            // format is (on|off) both in MS
+            String value = (String) newValue;
+            String[] breakIndex = value.split("\\|");
+            int onTime = Integer.valueOf(breakIndex[0]);
+            int offTime = Integer.valueOf(breakIndex[1]);
+
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_GLOW_DURATION[0], offTime);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_GLOW_DURATION[1], onTime);
+            updateGlowTimesSummary();
+            return true;
+        } else if (preference == mNavigationBarColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_TINT, intHex);
+            return true;
+        } else if (preference == mNavigationBarGlowColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_GLOW_TINT, intHex);
+            return true;
+        } else if (preference == mNavigationBarButtonColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_BUTTON_TINT, intHex);
+            return true; 
+	} else if (preference == mButtonAlpha) {
+            float val = Float.parseFloat((String) newValue);
+            Log.e("R", "value: " + val / 100);
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_BUTTON_ALPHA,
+                    val / 100);
+            return true;
+        } 
+        return false;
+    } 
+
+    private void updateGlowTimesSummary() {
+        int resId;
+        String combinedTime = Settings.System.getString(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_GLOW_DURATION[1]) + "|" +
+                Settings.System.getString(getContentResolver(),
+                        Settings.System.NAVIGATION_BAR_GLOW_DURATION[0]);
+
+        String[] glowArray = getResources().getStringArray(R.array.glow_times_values);
+
+        if (glowArray[0].equals(combinedTime)) {
+            resId = R.string.glow_times_superquick;
+            mGlowTimes.setValueIndex(0);
+        } else if (glowArray[1].equals(combinedTime)) {
+            resId = R.string.glow_times_quick;
+            mGlowTimes.setValueIndex(1);
+        } else {
+            resId = R.string.glow_times_normal;
+            mGlowTimes.setValueIndex(2);
+        }
+        mGlowTimes.setSummary(getResources().getString(resId));
+    }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
